c8867948ac86bef335f1bb8a52212883df4829e3
NaamaCahen <cahenbb@gmail.com>
2023-02-07 11:36:36 +0200

    xp starting python

---
+++ week6/day2/main.py
@@ -0,0 +1,25 @@
+print("Hello World \n" * 4)
+print((99 ** 3) * 8)
+computer_brand = 'Asus'
+print(f'I have an {computer_brand} computer')
+name = 'Naama'
+age = 19
+shoe_size = 37
+info = f"hi, my name is {name} , I am just {age} years old but already a fullstack developer, ha and i forgot telling " \
+       f"you that my shoe size is : {shoe_size}"
+print(info)
+a = 33
+b = 48
+if a > b:
+    print("Hello World")
+his_name=input("plese insert your name:")
+if name==his_name:
+    print("extra!! t'as un beau nom...:)")
+else:
+    print("dommage..")
+
+height=int(input("please insert your age in inches:"))
+if height>145:
+    print('you are tall enough to ride')
+else:
+    print('you need to grow some more to ride...')
