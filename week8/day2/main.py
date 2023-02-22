a92eb1cc1c8a683c5810f01a65994e4bcc009c2c
NaamaCahen <cahenbb@gmail.com>
2023-02-22 18:04:17 +0200

    small change

--- week8/day2/main.py
+++ week8/day2/main.py
@@ -7,14 +7,23 @@ class Farm:
         self.animals = {}
 
     def add_animal(self, animal_name: str, num: int = 1):
-        self.animals[animal_name] = num
+        if animal_name in self.animals.keys():
+            self.animals[animal_name] += num
+        else:
+            self.animals[animal_name] = num
     def get_info(self):
         return (f"""
         {self.farm_name}'s Farm
-            {for animal in self.animals:
-                
-            }
+        
+        {self.animals}
+        
+            I-E-I-E-0!
         """)
+    def get_animal_types(self):
+        return list(self.animals.keys())
+    def get_short_info(self):
+        types=self.get_animal_types()
+        return (f"{self.farm_name}'s farm has {types}")
 
 
 macdonald = Farm("McDonald")
@@ -23,3 +32,13 @@ macdonald.add_animal('sheep')
 macdonald.add_animal('sheep')
 macdonald.add_animal('goat', 12)
 print(macdonald.get_info())
+print(macdonald.get_animal_types())
+print(macdonald.get_short_info())
+
+# add a method called get_animal_types to the Farm class.
+# This method should return a sorted list of all the animal types (names) in the farm.
+# With the example above, the list should be: ['cow', 'goat', 'sheep'].
+
+# Add another method to the Farm class called get_short_info.
+# This method should return the following string: “McDonald’s farm has cows, goats and sheep.”.
+# The method should call the get_animal_types function to get a list of the animals.
