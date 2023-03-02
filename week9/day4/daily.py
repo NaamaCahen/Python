b7bd79299e5c844b232721b242bee85391494831
NaamaCahen <cahenbb@gmail.com>
2023-03-02 13:01:47 +0200

    daily challenge food

--- week9/day4/daily.py
+++ week9/day4/daily.py
@@ -1,19 +1,20 @@
 class Person:
-    def __init__(self,name,list_hate=[],list_like=[]):
-        self.name=name
-        self.list_hate_food=list_hate
-        self.list_like_food=list_like
+    def __init__(self, name, list_hate=[], list_like=[]):
+        self.name = name
+        self.list_hate_food = list_hate
+        self.list_like_food = list_like
 
-    def taste(self,food:str):
+    def taste(self, food: str):
         if food in self.list_like_food:
-            end_of_sentence='and likes it!'
+            end_of_sentence = 'and likes it!'
         elif food in self.list_hate_food:
-            end_of_sentence='and hates it!'
+            end_of_sentence = 'and hates it!'
         else:
-            end_of_sentence ='!'
+            end_of_sentence = '!'
         return f"{self.name} eats the {food} {end_of_sentence}"
 
-sam=Person('sam',['cheese','fish'])
+
+sam = Person('sam', ['cheese', 'fish'], ['chocolate'])
 print(sam.taste('cheese'))
 print(sam.taste('apple'))
 print(sam.taste('chocolate'))
